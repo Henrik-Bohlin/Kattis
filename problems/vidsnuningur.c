342462700e0283293759c957c8220d470be4da46
Henrik Bohlin <152969214+Henrik-Bohlin@users.noreply.github.com>
2024-07-25 10:32:37 +0200

    Create vidsnuningur.c

---
+++ problems/vidsnuningur.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <string.h>
+
+int main () {
+    
+    char input[1001];
+    scanf("%s", input);
+    int length = strlen(input);
+    for(int i = length-1;i>=0 ;i--) {
+        printf("%c", input[i]);
+    }
+    return 0; 
+}
