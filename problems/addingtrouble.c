72920ebc4186671437821ad9c1add7c98ecaf40c
Henrik Bohlin <152969214+Henrik-Bohlin@users.noreply.github.com>
2024-09-03 15:04:52 +0200

    Create addingtrouble.c

---
+++ problems/addingtrouble.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main () {
+    int A,B,C;
+    scanf("%d %d %d", &A, &B, &C);
+    if (A+B==C) {
+        printf("correct!");
+    } else {
+        printf("wrong!");
+    }
+    return 0;
+}
